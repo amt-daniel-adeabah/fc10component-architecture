3bfede5126a1d10e1ef42df02847d6831a8b3618
Daniel Adeabah <daniel.adeabah@amalitech.com>
2023-09-12 13:32:32 +0000

    feat: design the photo upload page

--- src/pages/PhotoPage.tsx
+++ src/pages/PhotoPage.tsx
@@ -1,15 +1,28 @@
-import "../assets/css/Photo.scss"
+import { useNavigate } from "react-router-dom";
 import SideMenu from '../components/SideMenu'
 import TopBar from '../components/TopBar'
+import BlankPhoto from "../assets/images/empty-image.svg"
+import "../assets/css/Photo.scss"
 
 const Photo = () => {
+  const navigate = useNavigate();
+
   return (
     <>
       <div className="sidemenu-container">
           <SideMenu />
         <div>
           <TopBar />
-          <div>Photo</div>
+          <div className="topbar-container">
+            <div className="topbar-column">
+              <h1>Add Photo</h1>
+              <p>Add a photo so other members know who you are</p>
+              <img src={BlankPhoto} alt="" />
+
+              <button className="upload"  onClick={() => navigate("/interest")}>Upload a photo</button>
+              <button className="skip"  onClick={() => navigate("/interest")}>Skip</button>
+            </div>
+          </div>
         </div>
       </div>
     </>
