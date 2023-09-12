0e6a917d19f9d22164a3e1aab0941cf357a565f4
Daniel Kusi Boateng <daniel.boateng@amalitech.org>
2023-09-12 08:55:25 +0000

    implement registration

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,19 +1,44 @@
-import "../assets/css/Register.scss"
-import SideMenu from '../components/SideMenu'
-import TopBar from '../components/TopBar'
+import { useNavigate } from "react-router-dom";
+import "../assets/css/Register.scss";
+import SideMenu from "../components/SideMenu";
+import TopBar from "../components/TopBar";
 
 const Register = () => {
+  const navigate = useNavigate();
   return (
     <>
-      <div className="sidemenu-container">
-          <SideMenu />
-        <div>
-          <TopBar />
-          <div>Register</div>
-        </div>
+      <SideMenu />
+      <div>
+        <TopBar />
+          <div className="account-creation">
+            <h4>Create Account</h4>
+            <form className="account-form" onSubmit={() => navigate('/photo')}>
+              <label>Name</label>
+              <input type="text" name="name" placeholder="John Doe" />
+              <label>E-mail</label>
+              <input
+                type="text"
+                name="e-mail"
+                placeholder="johndoe@gmail.com"
+              />
+              <label>Password</label>
+              <input
+                type="password"
+                name="password"
+                placeholder="Enter password"
+              />
+              <button type="submit" className="create-btn">
+                Create Account
+              </button>
+            </form>
+            <button className="sign-up">Sign up with Google</button>
+            <div className="login">
+              Already have an account? <a href="#">Log in</a>
+            </div>
+          </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
