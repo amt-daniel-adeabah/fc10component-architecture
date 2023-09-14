db98fa421b703f2233735b86f1a5b95e7f1eac0b
Daniel Adeabah <daniel.adeabah@amalitech.com>
2023-09-14 17:11:17 +0000

    refactor: change the app model

--- src/pages/Interest/InterestPage.tsx
+++ src/pages/Interest/InterestPage.tsx
@@ -3,6 +3,14 @@ import SideMenu from "../../components/SideMenu";
 import TopBar from "../../components/TopBar";
 import { FormPage } from "../../types/Types";
 
+const PageStates = {
+  Register: "register",
+  Photo: "photo",
+  Interest: "interest",
+  InterestPick: "interestPick",
+  InterestSuccess: "interestSuccess",
+};
+
 const interestOptions = [
   { label: "+ Social Interaction", className: "active-button" },
   { label: "+ Personal development", className: "interests-button" },
@@ -10,36 +18,58 @@ const interestOptions = [
   { label: "+ Rewards and recognition", className: "interests-button" },
 ];
 
-const InterestPage = ({formHomePage}:{formHomePage:React.Dispatch<React.SetStateAction<FormPage>>}) => {
+interface InterestButtonProps {
+  label: string;
+  className: string;
+}
+
+const InterestButton = ({ label, className }: InterestButtonProps) => (
+  <button className={className}>{label}</button>
+);
+
+const InterestPage = ({
+  formHomePage,
+}: {
+  formHomePage: React.Dispatch<React.SetStateAction<FormPage>>;
+}) => {
+  const currentPage = PageStates.Interest;
 
   return (
     <div className="sidemenu-container">
-      <SideMenu />
+      <SideMenu
+        registerPage={currentPage === PageStates.Register}
+        photoPage={currentPage === PageStates.Photo}
+        interestPage={currentPage === PageStates.Interest}
+        interestPick={currentPage === PageStates.InterestPick}
+        interestSuccess={currentPage === PageStates.InterestSuccess}
+      />
       <div>
-        <TopBar />
+      <TopBar currentPage="interestPage" />
         <div className="interest-container">
           <div className="interest-column">
             <h1>Hey there! What brings you here?</h1>
             <p>This will help us make great recommendations.</p>
 
-              {interestOptions.map((option, index) => (
-                <button
-                  key={index}
-                  className={option.className}
-                >
-                  {option.label}
-                </button>
-              ))}
+            {interestOptions.map((option, index) => (
+              <InterestButton
+                key={index}
+                label={option.label}
+                className={option.className}
+              />
+            ))}
 
-            <button className="continue" onClick={() =>
-                  formHomePage({
-                    register: false,
-                    photo: false,
-                    interest_page: false,
-                    interest_pick: true,
-                    interest_success: false,
-                  })
-                }>
+            <button
+              className="continue"
+              onClick={() =>
+                formHomePage({
+                  register: false,
+                  photo: false,
+                  interest_page: false,
+                  interest_pick: true,
+                  interest_success: false,
+                })
+              }
+            >
               Continue
             </button>
           </div>
--- src/pages/PhotoPage.tsx
+++ src/pages/PhotoPage.tsx
@@ -4,57 +4,70 @@ import BlankPhoto from "../assets/images/empty-image.svg";
 import "../components/css/Photo.scss";
 import { FormPage } from "../types/Types";
 
+const PageStates = {
+  Register: "register",
+  Photo: "photo",
+  Interest: "interest",
+  InterestPick: "interestPick",
+  InterestSuccess: "interestSuccess",
+};
+
 const PhotoPage = ({
   formHomePage,
 }: {
   formHomePage: React.Dispatch<React.SetStateAction<FormPage>>;
 }) => {
+  const currentPage = PageStates.Photo;
 
   return (
-    <>
-      <div className="sidemenu-container">
-        <SideMenu />
-        <div>
-          <TopBar />
-          <div className="photo-container">
-            <div className="photo-column">
-              <h1>Add Photo</h1>
-              <p>Add a photo so other members know who you are</p>
-              <img src={BlankPhoto} alt="" />
+    <div className="sidemenu-container">
+      <SideMenu
+        registerPage={currentPage === PageStates.Register}
+        photoPage={currentPage === PageStates.Photo}
+        interestPage={currentPage === PageStates.Interest}
+        interestPick={currentPage === PageStates.InterestPick}
+        interestSuccess={currentPage === PageStates.InterestSuccess}
+      />
+      <div>
+      <TopBar currentPage="photoPage" />
+        <div className="photo-container">
+          <div className="photo-column">
+            <h1>Add Photo</h1>
+            <p>Add a photo so other members know who you are</p>
+            <img src={BlankPhoto} alt="" />
 
-              <button
-                className="upload"
-                onClick={() =>
-                  formHomePage({
-                    register: false,
-                    photo: false,
-                    interest_page: true,
-                    interest_pick: false,
-                    interest_success: false,
-                  })
-                }
-              >
-                Upload a photo
-              </button>
-              <button
-                className="skip"
-                onClick={() =>
-                  formHomePage({
-                    register: false,
-                    photo: false,
-                    interest_page: true,
-                    interest_pick: false,
-                    interest_success: false,
-                  })
-                }
-              >
-                Skip
-              </button>
-            </div>
+            <button
+              className="upload"
+              onClick={() =>
+                formHomePage({
+                  register: false,
+                  photo: false,
+                  interest_page: true,
+                  interest_pick: false,
+                  interest_success: false,
+                })
+              }
+            >
+              Upload a photo
+            </button>
+            <button
+              className="skip"
+              onClick={() =>
+                formHomePage({
+                  register: false,
+                  photo: false,
+                  interest_page: true,
+                  interest_pick: false,
+                  interest_success: false,
+                })
+              }
+            >
+              Skip
+            </button>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
